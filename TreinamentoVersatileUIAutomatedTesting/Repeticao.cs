3ce9692413b6d597b853ca1d155c18e7d19f18b0
anderlinesantos <anderlinesantos@gmail.com>
2023-05-25 19:08:28 -0300

    News changes

---
+++ TreinamentoVersatileUIAutomatedTesting/Repeticao.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TreinamentoVersatileUIAutomatedTesting
+{
+    public class Repeticao
+    {
+        [Test]
+        public void Test2()
+        {
+            int index = 0;
+            // 1ºExemplo é um array
+            string[] nomes = { "Anderline", "Aline", "Alan", "Mari" };
+            while(index < nomes.Length)
+            {
+                Console.WriteLine("Imprimindo nomes While: " + nomes[index]);
+                index++;
+            }
+
+            for (var i = 0; i < nomes.Length; i++)
+            {
+                Console.WriteLine("Imprimindo nomes pelo FOR: " + nomes[i]);
+            }
+
+            foreach(var nome in nomes)
+            {
+                Console.WriteLine("Imprimindo nomes pelo FOREACH: " + nomes);
+            }
+
+            // 2º exemplo com o List
+            List<int> list = new List<int>();
+            index = 1;
+
+            while(index <= 10)
+            {
+                list.Add(index);
+                Console.WriteLine($"Adicionando o número inteiro: '{index}' na List");
+                index++;
+            }
+        }
+    }
+}
--- TreinamentoVersatileUIAutomatedTesting/Teste.cs
+++ TreinamentoVersatileUIAutomatedTesting/Teste.cs
@@ -1,22 +1,19 @@
 ﻿using NUnit.Framework;
-    /*
-     C#   Type    Valores possíveis de se armazenar
-          bool    Verdadeiro ou Falso (Valores booleanos)
-          char    Um caractere (16 bits)
-          string  Sequencia de caracteres (16 bits por caractere)
-          long    Sequencia de caracteres muito grandes (64 bits)
-          byte    0 a 255 (8 bits)
-          int     Sequencia de números inteiros (32 bits)
-          decimal Sequencia de números de ponto flutuante (128 bits)
-          double  Sequencia de números de ponto flutuante (64 bits)
-          float   Sequencia de números de ponto flutuante (32 bits)
-          object  Qualquer tipo
-     */
+/*
+C#   Type    Valores possíveis de se armazenar
+bool    Verdadeiro ou Falso (Valores booleanos)
+char    Um caractere (16 bits)
+string  Sequencia de caracteres (16 bits por caractere)
+long    Sequencia de caracteres muito grandes (64 bits)
+byte    0 a 255 (8 bits)
+int     Sequencia de números inteiros (32 bits)
+decimal Sequencia de números de ponto flutuante (128 bits)
+double  Sequencia de números de ponto flutuante (64 bits)
+float   Sequencia de números de ponto flutuante (32 bits)
+object  Qualquer tipo
+*/
 
-namespace TreinamentoVersatileUIAutomatedTesting
-{
-    public class Teste
-    {
+/*
         // Variável do tipo CHAR
         char InicialNomeDoMeio = 'A';
         object CharObject = 'A';
@@ -29,25 +26,72 @@ namespace TreinamentoVersatileUIAutomatedTesting
         //Variável do tipo Boolean
         bool EHomem = true;
         object boolObj = false;
+*/
+
+/*
+  // As variáveis do tipo "var" e "object" são variáveis coringa que podem receber qualquer tipo
+  string nomeString = "Anderline String";
+  var nomeVar = "Anderline Var";
+  object nomeObj = "Anderline Object";
+
+  int numeroInt = 1;
+  var numVar = 2;
+  object numObj = 3;
+
+  bool eHomem = false;
+  var eMulher = true;
+  object eObj = false;
+
+  Console.WriteLine($"Olá! Meu nome é {nomeString} e tenho  {numVar} anos! Será que eu sou mulher? {eMulher}");
+  */
+namespace TreinamentoVersatileUIAutomatedTesting
+{
+    public class Teste
+    {
+        public string Nome = "Aline";
+        public int Idade = 30;
+        public bool eMulher = true;
+
+        public string RetornaStringNome(string nome)
+        {
+            if (string.IsNullOrEmpty(nome))
+            {
+                return "O nome não foi informado!";
+            }
+            else if (!string.IsNullOrEmpty(nome) && nome.Equals("Anderline"))
+            {
+                return "Anderline é seu nome";
+            }
+            else if (!string.IsNullOrEmpty(nome) && nome.Equals("João") || nome.Equals("Jão"))
+            {
+                return "João é seu nome!";
+            }
+            else
+            {
+                return ($"Nome informado é diferente de Anderline e João, o nome informado foi {nome}");
+            } // fim do metodo
+        }
+
+        public string RetornarStringGenero(bool genero)
+        {
+            if (genero == true)
+            {
+                return "Feminino";
+            }
+            else
+            {
+                return "Masculino";
+            }
+        } // FIM DO METODO
 
         [Test]
-        public void Test()
+        public void Test1()
         {
-            // As variáveis do tipo "var" e "object" são variáveis coringa que podem receber qualquer tipo
-            string nomeString = "Anderline String";
-            var nomeVar = "Anderline Var";
-            object nomeObj = "Anderline Object";
-
-            int numeroInt = 1;
-            var numVar = 2;
-            object numObj = 3;
-
-            bool eHomem = false;
-            var eMulher = true;
-            object eObj = false;
-
-            Console.WriteLine($"Olá! Meu nome é {nomeString} e tenho  {numVar} anos! Será que eu sou mulher? {eMulher}");
-        }
-    }
-}
-
+            string recebeNome = RetornaStringNome(Nome);
+            string recebeGenero = RetornarStringGenero(eMulher);
+            Console.WriteLine(recebeNome);
+            Console.WriteLine(recebeGenero);
+        } //FIM DO METODO
+    
+    } //FIM DA CLASSE
+} // FIM DA NAMESPACE
